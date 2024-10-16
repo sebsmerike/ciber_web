f49fc6323d31a14f1b5770f432c7eb2f0bd7c96a
sebsmerike <150203111+sebsmerike@users.noreply.github.com>
2024-10-16 13:33:49 -0600

    Create 11_php_init.php
    Comienzo de 3er parcial

---
+++ parcial03/11_php_init.php
@@ -0,0 +1,17 @@
+<!-- Código de prueba para ejecutar un *echo* de PHP -->
+<html>
+    <head>
+        <title>Titulo</title>
+    </head>
+
+    <body>
+        <h1>Aloha!</h1>
+        <p><?php 
+            echo "Hello";
+            // Comentario de una línea
+            /* Comenta
+            rio de va
+            rias líneas*/
+        ?></p>
+    </body>
+</html>
